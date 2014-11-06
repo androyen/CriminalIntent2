a6359bb097a74db5b9dc0271dbb4dac9b616dc2c
Androyen <androyen@aol.com>
2014-11-05 19:32:29 -0600

    Initial Commit

---
+++ app/src/main/java/com/androyen/criminalintent/Crime.java
@@ -0,0 +1,16 @@
+package com.androyen.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by rnguyen on 11/5/14.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        
+    }
+}
