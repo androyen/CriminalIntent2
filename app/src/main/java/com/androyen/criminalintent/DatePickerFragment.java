9c3405932c1cf53e3b52009fb231d3b5da1ec665
Androyen <androyen@aol.com>
2014-11-06 18:17:10 -0600

    Got date from argument stash and convert it to Calendar object

--- app/src/main/java/com/androyen/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/androyen/criminalintent/DatePickerFragment.java
@@ -5,8 +5,11 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by rnguyen on 11/6/14.
@@ -18,9 +21,36 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //Get the date from the fragment arguments
+        mDate = (Date) getArguments().getSerializable(EXTRA_DATE);
+
+        //Create Calendar object to get year, month, and day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate); //Set calendar to the date from the fragment arguments bundle
+
+        //Get int values for the date from the calendar object
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
 
         //Inflate DatePicker widget
         View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
+        //Use int values of Calendar to set the DatePicker widget values
+        DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                //Translate year, month, day into a Date object using a calendar
+                mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+
+                //Update argument to preserve new date from screen rotation
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
 
 
         return new AlertDialog.Builder(getActivity())
