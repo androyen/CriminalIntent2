6516afba32515b3879660f468148542c0b25c168
Androyen <androyen@aol.com>
2014-11-10 12:13:02 -0600

    Hide the Action Bar and title when camera is opened

--- app/src/main/java/com/androyen/criminalintent/CrimeCameraActivity.java
+++ app/src/main/java/com/androyen/criminalintent/CrimeCameraActivity.java
@@ -1,6 +1,9 @@
 package com.androyen.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by rnguyen on 11/10/14.
@@ -10,5 +13,16 @@ public class CrimeCameraActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeCameraFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+
+        //Hide the action bar and title when camera is opened
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
     }
 }
